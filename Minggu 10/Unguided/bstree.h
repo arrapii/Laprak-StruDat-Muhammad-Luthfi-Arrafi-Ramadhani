4a2669716d698b73a0cf7f0759ccd9a62b6ac716
user123bvc45 <chocolatelava00@gmail.com>
2025-12-04 21:43:42 +0700

    Modul10_Fix

--- Minggu 10/Unguided/bstree.h
+++ Minggu 10/Unguided/bstree.h
@@ -2,3 +2,35 @@
 // 103112430043
 // IF 12-06
 
+#ifndef BSTREE_H
+#define BSTREE_H
+#include <iostream>
+using namespace std;
+
+#define Nil NULL
+
+typedef int infotype;
+
+struct Node {
+    infotype info;
+    Node* left;
+    Node* right;
+};
+
+typedef Node* address;
+
+address alokasi(infotype x);
+void insertNode(address &root, infotype x);
+address findNode(infotype x, address root);
+void InOrder(address root);
+
+// SOAL 2
+int hitungNode(address root);
+int hitungTotal(address root);
+int hitungKedalaman(address root, int start);
+
+// SOAL 3
+void PreOrder(address root);
+void PostOrder(address root);
+
+#endif
--- Minggu 10/Unguided/main.cpp
+++ Minggu 10/Unguided/main.cpp
@@ -1,3 +1,38 @@
 // Muhammad Luthfi Arrafi Ramadhani
 // 103112430043
 // IF 12-06
+
+#include <iostream>
+#include "bstree.h"
+#include "bstree.cpp"
+using namespace std;
+
+int main() {
+    cout << "Hello World!" << endl;
+
+    address root = Nil;
+
+    insertNode(root, 6);
+    insertNode(root, 4);
+    insertNode(root, 7);
+    insertNode(root, 2);
+    insertNode(root, 5);
+    insertNode(root, 1);
+    insertNode(root, 3);
+
+    // InOrder(root);
+    // cout << endl;
+    // // SOAL 2
+    // cout << "kedalaman : " << hitungKedalaman(root, 0) << endl;
+    // cout << "jumlah node : " << hitungNode(root) << endl;
+    // cout << "total : " << hitungTotal(root) << endl;
+
+    // SOAL 3
+    cout << "\npre-order: ";
+    PreOrder(root);
+
+    cout << "\npost-order: ";
+    PostOrder(root);
+
+    return 0;
+}
